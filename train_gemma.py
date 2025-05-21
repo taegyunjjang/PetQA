6109f31c805ac108febf92cbe102cf8d02784be9
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2025-05-21 15:07:29 +0900

    Update and rename train.py to train_gemma.py

--- train_gemma.py
+++ train_gemma.py
@@ -3,9 +3,8 @@ import torch
 import logging
 import wandb
 import argparse
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import TrainingArguments
-from peft import LoraConfig, PeftModel
 from datasets import load_dataset
 from dotenv import load_dotenv
 from trl import SFTTrainer
@@ -13,6 +12,7 @@ from trl import SFTTrainer
 
 MODEL_MAPPING = {
     "gemma-3-4b": "google/gemma-3-4b-it",
+    "qwen-2.5-7b": "Qwen/Qwen2.5-7B-Instruct",
     "exaone-3.5-7.8b": "LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct",
 }
 
@@ -20,17 +20,15 @@ def setup_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     return logging.getLogger(__name__)
 
-def load_environment(model_name, use_raw_format):
+def load_environment():
     load_dotenv()
-    hf_token = os.getenv("HF_TOKEN")
+    wandb.login(key=os.getenv("WANDB_API_KEY"))
     
-    suffix = "_raw" if use_raw_format else ""
-    cache_dir = "./models"
     output_dir = "./data/outputs"
-    final_model_dir = f"./data/outputs/{model_name}{suffix}"
+    train_dir = "./data/training"
 
-    train_path = f"./data/training/train_data{suffix}.json"
-    validation_path = f"./data/training/validation_data{suffix}.json"
+    train_path = os.path.join(train_dir, f"train.json")
+    validation_path = os.path.join(train_dir, f"validation.json")
     
     data_files = {
         "train": train_path,
@@ -38,10 +36,7 @@ def load_environment(model_name, use_raw_format):
     }
     
     return {
-        "hf_token": hf_token,
-        "cache_dir": cache_dir,
         "output_dir": output_dir,
-        "final_model_dir": final_model_dir,
         "data_files": data_files
     }
 
@@ -57,44 +52,25 @@ def load_datasets(data_files, logger):
     
     return train_data, validation_data
 
-def setup_model_config():
-    lora_config = LoraConfig(
-        r=16,
-        lora_alpha=16,
-        lora_dropout=0.05,
-        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
-        task_type="CAUSAL_LM",
-        bias="none"
-    )
-    
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
-    )
-    
-    return lora_config, bnb_config
-
-def load_model_tokenizer(model_name, bnb_config, cache_dir, logger):
+def load_model_tokenizer(model_name, logger):
     logger.info("모델 및 토크나이저 로드 중...")
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_MAPPING[model_name],
-        quantization_config=bnb_config,
         device_map="auto",
-        attn_implementation="eager",
-        cache_dir=cache_dir
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        attn_implementation="eager"
     )
     
     tokenizer = AutoTokenizer.from_pretrained(
         MODEL_MAPPING[model_name],
-        cache_dir=cache_dir,
     )
     
     tokenizer.padding_side = 'right'
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
     
-    tokenizer.model_max_length = model.config.text_config.max_position_embeddings
+    # tokenizer.model_max_length = model.config.text_config.max_position_embeddings  # Gemma3
     
     return model, tokenizer
 
@@ -103,24 +79,19 @@ def create_prompt_function(tokenizer, use_raw_format):
         system_prompt = "당신은 반려동물(개, 고양이) 의료 상담 전문 수의사입니다.\n당신의 역할은 개(강아지), 고양이 관련 의료 질문에 대해 유용하고, 완전하며, 전문적인 지식에 기반한 정확한 답변을 하는 것입니다.\n답변 외의 문장은 포함하지 마세요."
         
         if use_raw_format:
-            questions = [
-                f"{title}\n\n{content}" if content.strip() else title
-                for title, content in zip(data['title'], data['content'])
-            ]
+            question = f"{data['title']}\n\n{data['content']}" if data['content'].strip() else data['title']
         else:
-            questions = data['question']
+            question = data['preprocessed_question']
+        answer = data['preprocessed_answer']
         
-        prompts = []
-        for question, answer in zip(questions, data['answer']):
-            messages = [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": question},
-                {"role": "assistant", "content": answer}
-            ]
-            prompt = tokenizer.apply_chat_template(messages, tokenize=False)
-            prompts.append(prompt)
-            
-        return prompts
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": question},
+            {"role": "assistant", "content": answer}
+        ]
+        
+        prompt = tokenizer.apply_chat_template(messages, tokenize=False)
+        return prompt
     
     return generate_prompt
 
@@ -136,11 +107,13 @@ def setup_training_args(output_dir, train_data, use_raw_format):
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
         gradient_accumulation_steps=4,
-        optim="paged_adamw_8bit",
+        gradient_checkpointing=True,
+        # optim="paged_adamw_8bit",
         warmup_steps=int(0.1 * total_steps),
         learning_rate=2e-4,
         weight_decay=0.01,
-        fp16=True,
+        fp16=False,
+        bf16=True,
         logging_steps=50,
         eval_strategy="steps",
         eval_steps=500,
@@ -156,13 +129,12 @@ def setup_training_args(output_dir, train_data, use_raw_format):
     
     return training_args
 
-def train_model(model, train_data, validation_data, training_args, lora_config, formatting_func, logger):
+def train_model(model, train_data, validation_data, training_args, formatting_func, logger):
     trainer = SFTTrainer(
         model=model,
         train_dataset=train_data,
         eval_dataset=validation_data,
         args=training_args,
-        peft_config=lora_config,
         formatting_func=formatting_func,
     )
     
@@ -179,35 +151,17 @@ def save_best_model(trainer, output_dir, tokenizer, use_raw_format):
     tokenizer.save_pretrained(best_model_path)
     return best_model_path
 
-def merge_and_save_model(model_name, best_model_path, cache_dir, final_model_dir, tokenizer, logger):
-    logger.info("LoRA 어댑터와 기본 모델 병합 중...")
-    base_model = AutoModelForCausalLM.from_pretrained(
-        MODEL_MAPPING[model_name],
-        device_map="cpu",
-        cache_dir=cache_dir
-    )
-    
-    peft_model = PeftModel.from_pretrained(
-        base_model,
-        best_model_path,
-        device_map="cpu",
-        cache_dir=cache_dir
-    )
-    
-    merged_model = peft_model.merge_and_unload()
-    
-    logger.info(f"최종 병합 모델 저장 중: {final_model_dir}")
-    merged_model.save_pretrained(final_model_dir)
-    tokenizer.save_pretrained(final_model_dir)
-
 def main(model_name, use_raw_format):
     logger = setup_logging()
+    
+    env = load_environment()
     
     wandb.init(
         project="PetQA",
         entity="petqa",
         config={
             "model_name": MODEL_MAPPING[model_name],
+            "use_raw_format": use_raw_format,
             "learning_rate": 2e-4,
             "num_train_epochs": 3,
             "batch_size": 1,
@@ -215,13 +169,9 @@ def main(model_name, use_raw_format):
         }
     )
     
-    env = load_environment(model_name, use_raw_format)
-    
     train_data, validation_data = load_datasets(env["data_files"], logger)
     
-    lora_config, bnb_config = setup_model_config()
-    
-    model, tokenizer = load_model_tokenizer(model_name, bnb_config, env["cache_dir"], logger)
+    model, tokenizer = load_model_tokenizer(model_name, logger)
     
     generate_prompt = create_prompt_function(tokenizer, use_raw_format)
     
@@ -232,22 +182,12 @@ def main(model_name, use_raw_format):
         train_data, 
         validation_data, 
         training_args, 
-        lora_config, 
         generate_prompt,
         logger
     )
     
     best_model_path = save_best_model(trainer, env["output_dir"], tokenizer, use_raw_format)
-    
-    merge_and_save_model(
-        model_name,
-        best_model_path,
-        env["cache_dir"],
-        env["final_model_dir"],
-        tokenizer,
-        logger
-    )
-    
+    print(f"best_model_path: {best_model_path}")
     logger.info("프로세스 완료")
 
 
@@ -256,4 +196,5 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str, required=True, choices=list(MODEL_MAPPING.keys()))
     parser.add_argument("--use_raw_format", action="store_true")
     args = parser.parse_args()
+    
     main(args.model_name, args.use_raw_format)
